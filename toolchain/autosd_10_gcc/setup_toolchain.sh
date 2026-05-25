78ec18fcef8ca6f17642650b2b3f776c11230993
Leonardo Rossetti <lrossett@redhat.com>
2026-05-25 12:51:57 -0300

    libatomic linking
    Signed-off-by: Leonardo Rossetti <lrossett@redhat.com>

--- toolchain/autosd_10_gcc/setup_toolchain.sh
+++ toolchain/autosd_10_gcc/setup_toolchain.sh
@@ -277,7 +277,7 @@ echo "Setting up toolchain library directory..." >&2
 mkdir -p usr/lib64/toolchain
 
 shopt -s nullglob
-for lib_pattern in "libbfd*.so*" "libopcodes*.so*" "libctf*.so*" "libsframe*.so*" "libmpc.so*" "libgmp.so*" "libmpfr.so*" "libjansson.so*"; do
+for lib_pattern in "libbfd*.so*" "libopcodes*.so*" "libctf*.so*" "libsframe*.so*" "libmpc.so*" "libgmp.so*" "libmpfr.so*" "libjansson.so*" "libatomic*"; do
     for lib in usr/lib64/${lib_pattern}; do
         if [ -f "$lib" ]; then
             ln -sf "../$(basename "$lib")" usr/lib64/toolchain/
